b8d8b88ccd1216e64a84d10b9ffb0047a42ed121
salferre <salvatore.ferreri@eng.it>
2019-10-08 16:44:04 +0200

    login

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -11,14 +11,13 @@ public class LoginController {
     public static Boolean checkUser(String username, String password) {
 
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
             String jdbcUrl="jdbc:mysql://127.0.0.1:3306/StazioneFerroviaria?serverTimezone=UTC";
             Connection connection= DriverManager.getConnection(jdbcUrl, "root", "Suazami94");
-            String query = "SELECT * FROM Utenti WHERE username = ? AND password = ?";
+            String query = "SELECT * FROM Utenti u WHERE u.username = ? AND u.password = ?;";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, username);
             statement.setString(2, password);
-            ResultSet rs = statement.executeQuery(query);
+            ResultSet rs = statement.executeQuery();
             if (rs.next()) {
                 return true;
             }
